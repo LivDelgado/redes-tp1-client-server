9cd104976609d36ee2764c923535f67b7626c2d1
Lívia Delgado <23265664+LivDelgado@users.noreply.github.com>
2022-05-14 21:15:00 -0300

    fix: server is now really working with ipv6

--- server.c
+++ server.c
@@ -7,94 +7,115 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-static const int BUFSIZE = 500; // tamanho máximo da mensagem
-static const int MAX_PENDING = 5; // número máximo de solicitações de conexão
-static const int DEFAULT_PORT = 51511; // porta default de conexão
+static const int BUFSIZE = 500;             // tamanho máximo da mensagem
+static const int MAX_PENDING = 5;           // número máximo de solicitações de conexão
+static const int DEFAULT_PORT = 51511;      // porta default de conexão
 static const int ALLOW_IPV6_CONNECTION = 1; // flag de permissão para aceitar trabalhar com endereços ipv6
 
-
-void printErrorAndExit(char *errorMessage) {
+void printErrorAndExit(char *errorMessage)
+{
     puts(errorMessage);
     exit(1);
 }
 
-void validateAddressType(char *addressType) {
-    if (strcmp(addressType, "v4") != 0 && strcmp(addressType, "v6") != 0) {
+void validateAddressType(char *addressType)
+{
+    if (strcmp(addressType, "v4") != 0 && strcmp(addressType, "v6") != 0)
+    {
         printErrorAndExit("Invalid address type. It should be either v4 or v6.");
     }
-    if (strcmp(addressType, "v6") != 0 && !ALLOW_IPV6_CONNECTION) {
+    if (strcmp(addressType, "v6") != 0 && !ALLOW_IPV6_CONNECTION)
+    {
         printErrorAndExit("ipv6 is not supported.");
     }
 }
 
-int createIpv4TcpSocket() {
+int createIpv4TcpSocket()
+{
     int serverSocket;
-    if ((serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+    if ((serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    {
         printErrorAndExit("Failed to create ipv4 socket.");
     }
 
     return serverSocket;
 }
 
-int createIpv6TcpSocket() {
+int createIpv6TcpSocket()
+{
     int serverSocket;
-    if ((serverSocket = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+    if ((serverSocket = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    {
         printErrorAndExit("Failed to create ipv6 socket.");
     }
 
     return serverSocket;
 }
 
-int createTcpSocket(int addressIsV6) {
-    if (addressIsV6) {
+int createTcpSocket(int addressIsV6)
+{
+    if (addressIsV6)
+    {
         return createIpv6TcpSocket();
-    } else {
+    }
+    else
+    {
         return createIpv4TcpSocket();
     }
 }
 
-void bindToLocalIpv4Address(int serverSocket, int port) {
+void bindToLocalIpv4Address(int serverSocket, int port)
+{
     // cria struct de endereço local
-    struct sockaddr_in serverAddress; // endereço local
-    memset(&serverAddress, 0, sizeof(serverAddress)); // zero out structure
-    serverAddress.sin_family = AF_INET; // IPv4
+    struct sockaddr_in serverAddress;                  // endereço local
+    memset(&serverAddress, 0, sizeof(serverAddress));  // zero out structure
+    serverAddress.sin_family = AF_INET;                // IPv4
     serverAddress.sin_addr.s_addr = htonl(INADDR_ANY); // any incoming interface
-    serverAddress.sin_port = htons(port); // local port
+    serverAddress.sin_port = htons(port);              // local port
 
     // ligar ao endereço local
-    if (bind(serverSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0) {
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
         printErrorAndExit("Bind failed!");
         close(serverSocket);
     }
 }
 
-void bindToLocalIpv6Address(int serverSocket, int port) {
+void bindToLocalIpv6Address(int serverSocket, int port)
+{
     // cria struct de endereço local
-    struct sockaddr_in6 serverAddress; // endereço local
+    struct sockaddr_in6 serverAddress;                // endereço local
     memset(&serverAddress, 0, sizeof(serverAddress)); // zero out structure
-    serverAddress.sin6_family = AF_INET6; // IPv6
-    serverAddress.sin6_addr = in6addr_any; // any incoming interface
-    serverAddress.sin6_port = htons(port); // local port
+    serverAddress.sin6_family = AF_INET6;             // IPv6
+    serverAddress.sin6_addr = in6addr_any;            // any incoming interface
+    serverAddress.sin6_port = htons(port);            // local port
 
     // ligar ao endereço local
-    if (bind(serverSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0) {
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
         printErrorAndExit("Bind failed!");
         close(serverSocket);
     }
 }
 
-void bindToLocalAddress(int serverSocket, int port, int addressIsV6) {
-    if (addressIsV6) {
+void bindToLocalAddress(int serverSocket, int port, int addressIsV6)
+{
+    if (addressIsV6)
+    {
         bindToLocalIpv6Address(serverSocket, port);
-    } else {
+    }
+    else
+    {
         bindToLocalIpv4Address(serverSocket, port);
     }
 }
 
-void setTheServerToListen(int serverSocket, int port) {
+void setTheServerToListen(int serverSocket, int port)
+{
     printf("Trying to set the server to listen at port %i.\n", port);
 
-    if (listen(serverSocket, MAX_PENDING) < 0) {
+    if (listen(serverSocket, MAX_PENDING) < 0)
+    {
         printErrorAndExit("Server could not listen.");
         close(serverSocket);
     }
@@ -102,22 +123,28 @@ void setTheServerToListen(int serverSocket, int port) {
     printf("Server listening at port %i\n", port);
 }
 
-void handleClient(int clientSocket) {
+void handleClient(int clientSocket)
+{
     char buffer[BUFSIZE]; // buffer for echo string
 
     // Receive message from client
     ssize_t numBytesRcvd = recv(clientSocket, buffer, BUFSIZE, 0);
-    if (numBytesRcvd < 0) {
+    if (numBytesRcvd < 0)
+    {
         printErrorAndExit("recv() failed");
     }
 
     // Send received string and receive again until end of stream
-    while (numBytesRcvd > 0) { // 0 indicates end of stream
+    while (numBytesRcvd > 0)
+    { // 0 indicates end of stream
         // Echo message back to client
         ssize_t numBytesSent = send(clientSocket, buffer, numBytesRcvd, 0);
-        if (numBytesSent < 0) {
+        if (numBytesSent < 0)
+        {
             printErrorAndExit("send() failed");
-        } else if (numBytesSent != numBytesRcvd) {
+        }
+        else if (numBytesSent != numBytesRcvd)
+        {
             printErrorAndExit("send() sent unexpected number of bytes");
         }
 
@@ -130,24 +157,30 @@ void handleClient(int clientSocket) {
     close(clientSocket); // Close client socket
 }
 
-void handleIpv4(int serverSocket) {
-    while (1) {
+void handleIpv4(int serverSocket)
+{
+    while (1)
+    {
         struct sockaddr_in clientAddress;
-        
+
         socklen_t clientAddressLength = sizeof(clientAddress);
 
         // Wait for a client to connect
-        int clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientAddressLength);
-        if (clientSocket < 0) {
+        int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
+        if (clientSocket < 0)
+        {
             printErrorAndExit("Failed to accept connection from client.");
         }
-    
+
         puts("A client just connected!");
 
         char clientName[INET_ADDRSTRLEN]; // String to contain client address
-        if (inet_ntop(AF_INET, &clientAddress.sin_addr.s_addr, clientName, sizeof(clientName)) != NULL) {
+        if (inet_ntop(AF_INET, &clientAddress.sin_addr.s_addr, clientName, sizeof(clientName)) != NULL)
+        {
             printf("Handling client %s/%d\n", clientName, ntohs(clientAddress.sin_port));
-        } else {
+        }
+        else
+        {
             puts("Unable to get client address");
         }
 
@@ -155,24 +188,30 @@ void handleIpv4(int serverSocket) {
     }
 }
 
-void handleIpv6(int serverSocket) {
-    while (1) {
+void handleIpv6(int serverSocket)
+{
+    while (1)
+    {
         struct sockaddr_in6 clientAddress;
-        
+
         socklen_t clientAddressLength = sizeof(clientAddress);
 
         // Wait for a client to connect
-        int clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientAddressLength);
-        if (clientSocket < 0) {
+        int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
+        if (clientSocket < 0)
+        {
             printErrorAndExit("Failed to accept connection from client.");
         }
-    
+
         puts("A client just connected!");
 
         char clientName[INET6_ADDRSTRLEN]; // String to contain client address
-        if (inet_ntop(AF_INET6, &clientAddress.sin6_addr.s6_addr, clientName, sizeof(clientName)) != NULL) {
+        if (inet_ntop(AF_INET6, &clientAddress.sin6_addr.s6_addr, clientName, sizeof(clientName)) != NULL)
+        {
             printf("Handling client %s/%d\n", clientName, ntohs(clientAddress.sin6_port));
-        } else {
+        }
+        else
+        {
             puts("Unable to get client address");
         }
 
@@ -180,29 +219,36 @@ void handleIpv6(int serverSocket) {
     }
 }
 
-void handleConnections(int serverSocket, int addressIsV6) {
-    if (addressIsV6) {
+void handleConnections(int serverSocket, int addressIsV6)
+{
+    if (addressIsV6)
+    {
         handleIpv6(serverSocket);
-    } else {
+    }
+    else
+    {
         handleIpv4(serverSocket);
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // validando o número de argumentos
     // assumindo o formato do endereço como obrigatório e a porta como opcional
     // porta padrão é 51511
-    if (argc < 2 || argc > 3) {
-        printErrorAndExit("Invalid arguments. To run the server: server <address fornat> <port (optional)>");
+    if (argc < 2 || argc > 3)
+    {
+        printErrorAndExit("Invalid arguments. To run the server: server <address format> <port (optional)>");
     }
 
     char *addressType = argv[1]; // primeiro argumento -> v4 ou v6
     validateAddressType(addressType);
-    int addressIsV6 = (addressType == "v6");
+    int addressIsV6 = (strcmp(addressType, "v6") == 0);
     printf("Address version is v6? %i\n", addressIsV6);
 
     int port = DEFAULT_PORT;
-    if (argc == 3) {
+    if (argc == 3)
+    {
         port = atoi(argv[2]); // segundo argumento (opcional) -> porta
     }
 
